dcc94304a79c82a66db67b5adf93ff5368076359
Matthew Good <matthewgood@gatech.edu>
2015-12-06 18:23:54 -0600

    Import scripts work, db create works.

---
+++ CatCat/init_data/db_init.py
@@ -0,0 +1,45 @@
+import csv
+from CatCat.models import User, Image, Location, Mention, TwitterLog
+from geoalchemy2.elements import WKTElement
+import os
+
+def load_images(db):
+    print("loading images...")
+    with open('init_data\legacy_export\Image.csv', 'r') as img_csvfile:
+        reader = csv.DictReader(img_csvfile)
+        for row in reader:
+            #ignore mentions for now, handle below if you need them
+            if row['mention_id'] == "NULL":
+                entry_date = row['entry_date']
+                filename = row['filename']
+                address_text = row['address_text']
+                title = row['title']
+                description = row['description']
+                verified = row['verified']
+                rejected = row['rejected']
+                loc_wkt = row['loc_wkt']
+                il = Location()
+                il.loc = WKTElement(loc_wkt)
+
+                i = Image()
+                i.entry_date = entry_date
+                i.filename = filename
+                i.address_text = address_text
+                i.title = title
+                i.description = description
+                i.verified = verified
+                i.rejected = rejected
+
+                i.location = il
+                db.session.add(i);
+    print("committing...")
+    db.session.commit()
+    print("done")
+
+#should I even bother with twitter or just start fresh?
+def load_twitter_log(db):
+    pass
+
+def load_mentions(db):
+    #handle mentions, and their associated images (excluded from load_images above)
+    pass
--- CatCat/manage.py
+++ CatCat/manage.py
@@ -6,6 +6,7 @@ from geoalchemy2 import *
 from CatCat.models import Image, Location
 from bot import reader
 import logging
+from init_data import db_init
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
@@ -18,45 +19,10 @@ manager.add_command("shell", Shell(make_context=make_shell_context))
 def init_db():
     db.drop_all()
     db.create_all()
-
-@manager.command
-def old_copy_data():
-    thing = input("Press enter to start")
-    #copy over initial data from catcat v1
-    #old_query = text("""insert into catcat2.Image
-    #    (entry_date, filename, address_text, loc, title, description, verified, rejected)
-    #    select ml.entry_date, mls.filename, ml.address, PointFromText( CONVERT(AsText(ml.address_loc),CHAR(64)) ), ml.address, ml.comments, ml.approved, 0
-    #    from mapmap.location ml
-	   #     join mapmap.location_size mls on ml.location_id=mls.location_id and mls.size_code='ORIGINAL'
-    #    where ml.approved=1;""");
-    #Since DB refactoring to a Location table, single insert no longer works :(
-    q = text("""select ml.entry_date, mls.filename, ml.address,
-        AsText(ml.address_loc) loc_txt,
-        ml.comments, ml.approved
-        from mapmap.location ml
-	        join mapmap.location_size mls on ml.location_id=mls.location_id and mls.size_code='ORIGINAL'
-        where ml.approved=1;""")
-    v1_images = db.session.execute(q).fetchall()
-    for i in v1_images:
-        #loc
-        il = Location()
-        wkt = i['loc_txt']
-        il.loc = WKTSpatialElement(wkt)
-        #image
-        img = Image()
-        img.entry_date = i['entry_date']
-        img.filename = i['filename']
-        img.address_text = i['address']
-        img.title = i['address']
-        img.description = i['comments']
-        img.verified = i['approved'] == '1'
-        img.rejected = 0
-        
-        img.location = il
-        #il.image = img
-        db.session.add(img);
     
-    db.session.commit()
+@manager.command
+def import_data():
+    db_init.load_images(db)
 
 @manager.command
 def test_mentions():
