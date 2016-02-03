176c0190af78e5736476c61e773dcf05c919ec48
Matthew Good <matthewgood@gatech.edu>
2016-02-02 19:20:46 -0600

    prefer https config option

--- CatCat/CatCat/config_example.py
+++ CatCat/CatCat/config_example.py
@@ -5,6 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     WTF_CSRF_ENABLED = True
     SECRET_KEY = os.environ.get('SECRET_KEY') or "some key"
+    PREFERRED_URL_SCHEME = 'https'
     
     SQLALCHEMY_COMMIT_ON_TEARDOWN = False
     SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
